7707a99aa150ced23895a94e2462d34faefda67e
JustKarlos <136405891+JustKarlos@users.noreply.github.com>
2024-11-25 21:46:48 -0300

    Update items.py

--- app/routers/items.py
+++ app/routers/items.py
@@ -12,14 +12,14 @@ def get_items () :
 def get_item ( item_id : int) :
   if item_id not in items_db :
     raise HTTPException ( status_code =404 , detail =" Item no,→ encontrado ")
-return items_db [ item_id ]
+    return items_db [ item_id ]
 
 @router . post ("/ items ", summary =" Crear un nuevo tem ")
 def create_item ( item_id : int , item : Item ) :
   if item_id in items_db :
     raise HTTPException ( status_code =400 , detail ="ID ya,→ existe ")
     items_db [ item_id ] = item . dict ()
-return {" message ": " tem creado ", " item ": item }
+    return {" message ": " tem creado ", " item ": item }
 
 @router . put ("/ items /{ item_id }", summary =" Actualizar un tem,→ existente ")
 def update_item ( item_id : int , item : Item ) :
